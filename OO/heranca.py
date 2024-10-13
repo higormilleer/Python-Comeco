063cfa7b45ecf45eb123b6a481956e91ebb48618
Higor Miller Grassi <higormiller55@gmail.com>
2024-10-13 12:57:51 -0300

    Update heranca.py

--- OO/heranca.py
+++ OO/heranca.py
@@ -21,11 +21,15 @@ class Ferrari(Carro):
     def acelerar(self):
         super().acelerar()
         return super().acelerar()
+    def turbo(self):
+        self._Carro__velocidade += 30
+        return f"Modo Turbo ativado! Velocidade: {self._Carro__velocidade} km/h"
 
 c1 = Ferrari()
 print(c1.acelerar())
 print(c1.acelerar())
 print(c1.acelerar())
+print(c1.turbo())
 print(c1.frear())
 print(c1.frear())
 print(c1.frear())
