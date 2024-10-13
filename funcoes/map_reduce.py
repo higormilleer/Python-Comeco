f749ad90dcfa9dd47cecc11485380be785ea3de8
Higor Miller Grassi <higormiller55@gmail.com>
2024-10-13 12:57:49 -0300

    Update map_reduce.py

--- funcoes/map_reduce.py
+++ funcoes/map_reduce.py
@@ -34,3 +34,18 @@ print(notas_finais)
 
 #for i in range(len(notas)):
 #    notas[i] = notas[i]+1.5
+
+
+celsius_para_fahrenheit = lambda c: (c * 9/5) + 32
+
+temperaturas_celsius = [-5, 0, 10, 20, 30, 35, 40]
+
+temperaturas_fahrenheit = map(celsius_para_fahrenheit, temperaturas_celsius)
+
+temperaturas_fahrenheit = list(temperaturas_fahrenheit)
+
+verificar_temperatura = lambda temp: "congelando" if temp < 32 else "muito quente" if temp > 95 else "normal"
+
+print("Temperaturas em Celsius:", temperaturas_celsius)
+print("Temperaturas em Fahrenheit:", temperaturas_fahrenheit)
+print("Status das temperaturas:", verificar_temperatura)
